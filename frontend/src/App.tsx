3ab1f883b5b5b00e4a1a91719aaabeb9d14c3432
kenvynKwek <kenvynkwek@gmail.com>
2024-09-19 21:56:50 +0800

    Revert "Resolve merge conflicts"
    This reverts commit bc5d54ce9f98fea24fb0157a961695e9b54df429, reversing
    changes made to bdd1265054ccf752f9a7a336c0bf341fbb944f68.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePageView from "./views/HomePageView";
 import LoginPage from "./views/LoginPageView";
+import LoginSuccessPage from "./views/LoginSuccessPageView";
 import QuestionPageView from "./views/QuestionPageView";
 import CreateAccountPageView from "./views/CreateAccountPageView";
-import CollabPageView from "./views/CollabPageView";
-import DeleteAccountPage from "./views/DeleteAccountPageView";
 
 const App: React.FC = () => {
   return (
@@ -14,10 +13,9 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePageView />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/login-success" element={<LoginSuccessPage />} />
         <Route path="/questions" element={<QuestionPageView />} />
         <Route path="/create-account" element={<CreateAccountPageView />} />
-        <Route path="/collab" element={<CollabPageView />} />
-        <Route path="/delete-account" element={<DeleteAccountPage />} />
         <Route path="*" element={<p>404: Page Not Found!</p>} />
       </Routes>
     </BrowserRouter>
