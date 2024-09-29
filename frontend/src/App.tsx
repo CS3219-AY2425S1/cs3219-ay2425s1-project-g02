43db124de5337d5ff2db20ddaa3b911ceb2f7356
Tan-Jia-Rong <tanjiarongtjr@gmail.com>
2024-09-29 18:52:13 +0800

    Remove D3 changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,8 @@ import HomePageView from "./views/HomePageView";
 import LoginPage from "./views/LoginPageView";
 import QuestionPageView from "./views/QuestionPageView";
 import CreateAccountPageView from "./views/CreateAccountPageView";
-import CollabPageView from "./views/CollabPageView";
 import DeleteAccountPage from "./views/DeleteAccountPageView";
-import ProtectedRoute from "@/components/custom/ProtectedRoute/ProtectedRoute"; 
+import ProtectedRoute from "@/components/custom/ProtectedRoute/ProtectedRoute";
 
 const App: React.FC = () => {
   return (
@@ -14,33 +13,22 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePageView />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route 
-          path="/questions" 
+        <Route
+          path="/questions"
           element={
             <ProtectedRoute>
               <QuestionPageView />
             </ProtectedRoute>
-          } 
+          }
         />
-        <Route 
-          path="/create-account" 
-          element={<CreateAccountPageView />} 
-        />
-        <Route 
-          path="/collab" 
-          element={
-            <ProtectedRoute>
-              <CollabPageView />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/delete-account" 
+        <Route path="/create-account" element={<CreateAccountPageView />} />
+        <Route
+          path="/delete-account"
           element={
             <ProtectedRoute>
               <DeleteAccountPage />
             </ProtectedRoute>
-          } 
+          }
         />
         <Route path="*" element={<p>404: Page Not Found!</p>} />
       </Routes>
